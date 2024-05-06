df751cd739f31c3d8347d92fcfaf0a3154f7af9f
Giorgio Garofalo <giogar2705@gmail.com>
2024-05-06 12:00:51 +0200

    Add search placeholder

--- cinematic/View/SearchView.swift
+++ cinematic/View/SearchView.swift
@@ -10,22 +10,33 @@ struct SearchView: View {
     
     var body: some View {
         NavigationStack {
-            MovieCollectionShowcase(movies: vm.results, type: .grid)
-                .navigationTitle("search.title")
-                .navigationBarTitleDisplayMode(vm.query.isEmpty ? .large : .inline)
-                .animation(.easeIn, value: vm.results)
-                .padding(.horizontal)
-                .background(Color.background.ignoresSafeArea())
-                .searchable(text: $vm.query, prompt: "search.query.prompt")
-                .onChange(of: vm.query) { 
-                    // Min query length
-                    if vm.query.count >= SearchView.minQueryLength {
-                        Task {
-                            await vm.perform()
-                        }
-                        user.addTrophy(Trophies.search)
-                    }
+            Group {
+                if vm.query.isEmpty {
+                    // Placeholder
+                    ContentUnavailableView(
+                        "search.empty.title",
+                        systemImage: "magnifyingglass",
+                        description: Text("search.empty.subtitle")
+                    )
+                } else {
+                    MovieCollectionShowcase(movies: vm.results, type: .grid)
                 }
+            }
+            .navigationTitle("search.title")
+            .navigationBarTitleDisplayMode(vm.query.isEmpty ? .large : .inline)
+            .animation(.easeIn, value: vm.results)
+            .padding(.horizontal)
+            .background(Color.background.ignoresSafeArea())
+            .searchable(text: $vm.query, prompt: "search.query.prompt")
+            .onChange(of: vm.query) {
+                // Min query length
+                if vm.query.count >= SearchView.minQueryLength {
+                    Task {
+                        await vm.perform()
+                    }
+                    user.addTrophy(Trophies.search)
+                }
+            }
         }
     }
 }
